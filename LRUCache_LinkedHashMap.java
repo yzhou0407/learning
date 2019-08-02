dcf32f5db5d06e3ac88580b4e4f7e257ffcf9da3
yzhou0407 <38146164+yzhou0407@users.noreply.github.com>
2019-08-01 23:08:16 -0700

    Create LRUCache_LinkedHashMap.java

---
+++ LRUCache_LinkedHashMap.java
@@ -0,0 +1,32 @@
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+    
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        // initialize the linked hash map with capacity, 0.75F load factor and access order set to true;
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+        
+    }
+    
+    public void put(int key, int value) {
+        super.put(key,value);
+        
+    }
+    
+     @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity; 
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
